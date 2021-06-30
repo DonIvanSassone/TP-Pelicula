7b46aae5ecc38590d82d546170fd909fadea5021
DonIvanSassone <ivansassone123@outlook.es>
2021-06-30 17:41:34 -0300

    Commit avanzado

--- films/models.py
+++ films/models.py
@@ -3,6 +3,22 @@ from django.utils import timezone
 
 
 class FilmsToWatch(models.Model):
-    Original_name = models.CharField(max_length=200)
+    Originalname = models.CharField(max_length = 200)
+    Year = models.DateTimeField()
     Sinopsis = models.TextField()
-    Year = models.DateTimeField(default=timezone.now)
+    Director = models.CharField(max_length = 30)
+    Duration = models.CharField(max_length = 6)
+    Genres = models.CharField(max_length = 200)
+    RATE_CHOICES = (
+        (1,"1"),
+        (2,"2"),
+        (3,"3"),
+        (4,"4"),
+        (5,"5"),
+    )
+    rate = models.PositiveSmallIntegerField(choices = RATE_CHOICES, default = 0)
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    def __str__(self):
+        return self.Originalname
